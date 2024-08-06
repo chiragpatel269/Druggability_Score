8b8f64efbd25cf3287db5803806980f1f926800d
raghvendra5688 <raghvendra5688@gmail.com>
2024-08-06 15:13:48 +0400

    Adding consensus druggability matrix from ML techniques

--- scripts/drugpurpose_predictions.py
+++ scripts/drugpurpose_predictions.py
@@ -17,7 +17,9 @@ import pandas as pd
 from DeepPurpose import utils
 from DeepPurpose import DTI as models
 import numpy as np
+import torch
 import time
+import gc
 
 # +
 #Make the function to get dti virtual screening
@@ -25,7 +27,7 @@ common_path = '/home/Raghvendra.Mall/TII/Projects/Raghav/CAR-T/Druggability_Scor
 
 def DTI_pred(drugs, targets, model_name, drug_names, target_names):
     model = models.model_pretrained(common_path+"Models/pretrained_models/"+model_name)
-    model.config['batch_size']=4096
+    model.config['batch_size']=8192
     print(model.config)
 
     #Get predictions using virtual screening
@@ -74,7 +76,7 @@ model_names = ["CNN_CNN_BindingDB_IC50","MPNN_CNN_DAVIS","Morgan_CNN_BindingDB_I
                "Daylight_AAC_DAVIS","Daylight_AAC_KIBA","Daylight_AAC_BindingDB","Morgan_AAC_BindingDB","Morgan_AAC_KIBA","Morgan_AAC_DAVIS"]
 
 #Already ran code for CNN_CNN_BindingDB
-for i in range(1,len(model_names)):
+for i in range(15,len(model_names)):
     model_name = model_names[i]
     start_time = time.time()
     predictions = DTI_pred(revised_drug_smiles, revised_protein_seqs, model_name, revised_drug_names, revised_protein_names)
@@ -83,6 +85,8 @@ for i in range(1,len(model_names)):
     #Move the results accordingly
     os.system("mv ../Results/"+model_name+"/virtual_screening.txt ../Results/"+model_name+"/"+model_name+"_virtual_screening.csv")
     os.system("mv ../Results/"+model_name+" ../../General_Data/virtual_screenings/")
+    torch.cuda.empty_cache()
+    gc.collect()
 
 # -
 
